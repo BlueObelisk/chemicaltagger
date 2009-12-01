6c7e3c1899338a4657497011221f6ba73f527ef3
lh359 <lh359@cam.ac.uk>
2009-12-01 13:44:21 +0000

    Added some moe corrections like ignoring OSCAR-ONT and the tagtuple module in POSContainer

--- src/main/java/uk/ac/cam/ch/wwmm/extractPhrases/POSContainer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/extractPhrases/POSContainer.java
@@ -6,107 +6,101 @@ package uk.ac.cam.ch.wwmm.extractPhrases;
  * 
  * @author lh359
  ********************************************/
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import uk.ac.cam.ch.wwmm.extractPhrases.WWMMTag.TagType;
 
-
 public class POSContainer {
-	public List<String> wordTokenList = new ArrayList<String>();
-	public List<WWMMTag> oscarTagList = new ArrayList<WWMMTag>();
-	public List<WWMMTag> regexTagList = new ArrayList<WWMMTag>();
-	public List<WWMMTag> brownTagList = new ArrayList<WWMMTag>();
-	private List<WWMMTag> combinedTagsList = new ArrayList<WWMMTag>();
-	private final Logger LOG = Logger.getLogger(POSContainer.class);
-        private static String SPACE = " ";
-	public POSContainer() {
-		
-	}
 
-        public String getTokenTagTupleAsString(){
-             StringBuilder tokenTagTupleString = new StringBuilder();
+    public List<String> wordTokenList = new ArrayList<String>();
+    public List<WWMMTag> oscarTagList = new ArrayList<WWMMTag>();
+    public List<WWMMTag> regexTagList = new ArrayList<WWMMTag>();
+    public List<WWMMTag> brownTagList = new ArrayList<WWMMTag>();
+    private List<WWMMTag> combinedTagsList = new ArrayList<WWMMTag>();
+    private final Logger LOG = Logger.getLogger(POSContainer.class);
+    private static String SPACE = " ";
 
-             for (int i = 0; i < wordTokenList.size(); i++) {
-                  tokenTagTupleString.append(combinedTagsList.get(i).POS);
-                  tokenTagTupleString.append(SPACE);
-                  tokenTagTupleString.append(wordTokenList.get(i));
-                  tokenTagTupleString.append(SPACE);
-             }
+    public POSContainer() {
+    }
 
-             System.out.print(tokenTagTupleString.toString().trim());
-             return tokenTagTupleString.toString().trim();
+    public String getTokenTagTupleAsString() {
+        StringBuilder tokenTagTupleString = new StringBuilder();
+
+        for (int i = 0; i < wordTokenList.size(); i++) {
+            if (StringUtils.isNotEmpty(combinedTagsList.get(i).POS) && StringUtils.isNotEmpty(wordTokenList.get(i))) {
+                tokenTagTupleString.append(combinedTagsList.get(i).POS);
+                tokenTagTupleString.append(SPACE);
+                tokenTagTupleString.append(wordTokenList.get(i));
+                tokenTagTupleString.append(SPACE);
+            }
+        }
+
+
+        return tokenTagTupleString.toString().trim();
+
+    }
+
+    public void addToTokenList(String token) {
+        wordTokenList.add(token);
+    }
+
+    public List<String> getTokenList() {
+        return wordTokenList;
+    }
+
+    public void addToOSCARList(String oscarTag) {
+        oscarTagList.add(new WWMMTag("OSCAR-" + oscarTag));
+
+
+    }
+
+    public void addToOSCARList(String oscarTag, Enum type) {
+        oscarTagList.add(new WWMMTag("OSCAR-" + oscarTag, (TagType) type));
+    }
+
+    public void addToRegexList(String regexTag) {
+        regexTagList.add(new WWMMTag(regexTag));
+
+    }
+
+    public void addToBrownListFromStringArray(String[] brownTags) {
+        for (String string : brownTags) {
+            brownTagList.add(new WWMMTag(string));
+        }
+    }
+
+    public List<WWMMTag> getCombinedTagsList() {
+        return combinedTagsList;
+    }
+
+    public void combineTaggers() {
+        for (int i = 0; i < oscarTagList.size(); i++) {
+            if (!oscarTagList.get(i).getPOS().toLowerCase().equals("oscar-nil") && !oscarTagList.get(i).getPOS().toLowerCase().equals("oscar-ont")) {
+                combinedTagsList.add(oscarTagList.get(i));
+            } else if (!regexTagList.get(i).getPOS().equals("nil")) {
+                combinedTagsList.add(regexTagList.get(i));
+            } else {
+                combinedTagsList.add(brownTagList.get(i));
+            }
+        }
+
+    }
+
+    public void printOutTags() {
+
+        System.out.println("Token List is " + this.wordTokenList);
+        for (int i = 0; i < wordTokenList.size(); i++) {
+            System.out.println("WORD:" + wordTokenList.get(i));
+            System.out.println("\t>>OSCAR TAG: " + oscarTagList.get(i).POS);
+            System.out.println("\t>>REGEX TAG: " + regexTagList.get(i).POS);
+            System.out.println("\t>>OPENNLP TAG: " + brownTagList.get(i).POS);
+            System.out.println("\t>>COMBINED TAG: " + combinedTagsList.get(i).POS);
 
         }
 
-	public void addToTokenList(String token) {
-		wordTokenList.add(token);
-	}
-
-	public List<String> getTokenList() {
-		return wordTokenList;
-	}
-
-	public void addToOSCARList(String oscarTag) {
-                    oscarTagList.add(new WWMMTag("OSCAR-"+oscarTag));
-                
-                
-	}
-
-	public void addToOSCARList(String oscarTag, Enum type) {
-		oscarTagList.add(new WWMMTag("OSCAR-"+oscarTag, (TagType) type));
-	}
-
-	public void addToRegexList(String regexTag) {
-		regexTagList.add(new WWMMTag(regexTag));
-
-	}
-
-	public void addToBrownListFromStringArray(String[] brownTags) {
-		for (String string : brownTags) {
-			brownTagList.add(new WWMMTag(string));
-		}
-	}
-
-	public List<WWMMTag> getCombinedTagsList() {
-		return combinedTagsList;
-	}
-
-
-	public void combineTaggers() {
-		for (int i = 0; i < oscarTagList.size(); i++) {
-			if (!oscarTagList.get(i).getPOS().toLowerCase().equals("oscar-nil")) {
-				combinedTagsList.add(oscarTagList.get(i));
-			} else if (!regexTagList.get(i).getPOS().equals("nil")) {
-				combinedTagsList.add(regexTagList.get(i));
-			} else {
-				combinedTagsList.add(brownTagList.get(i));
-			}
-		}
-
-	}
-	
-	public void printOutTags() {
-
-		System.out.println("Token List is " + this.wordTokenList);
-		for (int i = 0; i < wordTokenList.size(); i++) {
-			System.out.println("WORD:" + wordTokenList.get(i));
-			System.out.println("\t>>OSCAR TAG: " + oscarTagList.get(i).POS);
-			System.out.println("\t>>REGEX TAG: " + regexTagList.get(i).POS);
-			System.out.println("\t>>OPENNLP TAG: " + brownTagList.get(i).POS);
-			System.out.println("\t>>COMBINED TAG: " + combinedTagsList.get(i).POS);
-
-		}
-
-	}
-
-
-	
-
-
-
-
+    }
 }
