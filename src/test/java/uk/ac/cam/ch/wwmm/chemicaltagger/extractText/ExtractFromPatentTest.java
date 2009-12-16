c70e585c7edc0b2de8b8df0897c58662a03e01cd
keybo <keybo@BabyBlue>
2009-12-16 15:44:39 +0000

    added extractFromPatent test class .

--- src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/extractText/ExtractFromPatentTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/extractText/ExtractFromPatentTest.java
@@ -7,13 +7,19 @@ import org.junit.Test;
 
 public class ExtractFromPatentTest {
 
-	@Before
+	/*@Before
 	public void setUp() throws Exception {
-	}
+	}*/
 
 	@Test
 	public void testGetContent() {
-		fail("Not yet implemented");
-	}
+            String resourcePath = "src/test/resources/patents/paragraph0018.xml";
+
+            ExtractFromPatent extract = new ExtractFromPatent();
+            DocumentContainer docContainer = extract.getContent(resourcePath);
+            String patentContent = docContainer.getContent();
+            System.out.println(patentContent);
+
+        }
 
 }
