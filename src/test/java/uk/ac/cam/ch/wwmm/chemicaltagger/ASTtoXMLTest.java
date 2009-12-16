ff7a667522b93d1a479a5aaadb9427e122a37963
keybo <keybo@BabyBlue>
2009-12-16 15:38:41 +0000

    added test classes (for real this time)

---
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/ASTtoXMLTest.java
@@ -0,0 +1,29 @@
+package uk.ac.cam.ch.wwmm.chemicaltagger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ASTtoXMLTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testASTtoXML() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNodes() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/extractText/ExtractFromPatentTest.java
@@ -0,0 +1,19 @@
+package uk.ac.cam.ch.wwmm.chemicaltagger.extractText;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExtractFromPatentTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetContent() {
+		fail("Not yet implemented");
+	}
+
+}
