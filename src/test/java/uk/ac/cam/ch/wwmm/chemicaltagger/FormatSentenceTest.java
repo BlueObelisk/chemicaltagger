0f412b90f10b2d8709e0e07d5a2909469fb63df4
lh359 <lh359@cam.ac.uk>
2010-01-08 03:49:41 +0000

    added regex tests, possibly solved issue with encoding

--- src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/FormatSentenceTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/FormatSentenceTest.java
@@ -16,9 +16,9 @@ public class FormatSentenceTest {
 	@Test	
 	public void sentence1() {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/formatTest/sentence1.txt");
-
+        String cleanSentence = Utils.formatSentence(sentence);
 		String ref = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/formatTest/ref1.txt");
-		Assert.assertEquals(ref,Utils.formatSentence(sentence));
+		Assert.assertEquals(ref,cleanSentence);
 
 
 	}
