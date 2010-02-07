900832dbe8cf9e35ece3fa79ac0fa35335911a21
dmj30 <dmj30@cam.ac.uk>
2010-02-07 17:27:29 +0000

    fixed problem of spawning many copies of tag.bin

--- src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/OpenNLPTagger.java
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/OpenNLPTagger.java
@@ -3,6 +3,7 @@ package uk.ac.cam.ch.wwmm.chemicaltagger;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -15,6 +16,27 @@ import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
 public class OpenNLPTagger {
+
+	private static File tempFile;
+	
+	static {
+		 try {
+			//the constructor only takes a file path, so we copy it to temp in case
+			//the code is running from a jar
+			//openNlp throws a weird NumberFormatException if the temp file name doesn't end in .bin (!)			
+			tempFile = File.createTempFile("tag", ".bin");
+			tempFile.deleteOnExit();
+			//TODO ensure temp files are properly deleted
+			InputStream is = ClassLoader.getSystemResourceAsStream("openNlpResources/tag.bin");
+			OutputStream os = new FileOutputStream(tempFile);
+			IOUtils.copy(is, os);
+		}
+		 catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	private final Logger LOG = Logger.getLogger(OscarTagger.class);
 
 	/****************************
@@ -42,16 +64,6 @@ public class OpenNLPTagger {
 			InputStreamReader tagDictReader = new InputStreamReader(
 					ClassLoader.getSystemResourceAsStream("openNlpResources/tagdict"), "UTF-8");
 			tagDict = new POSDictionary(new BufferedReader(tagDictReader), true);
-
-			//the constructor only takes a file path, so we copy it to temp in case
-			//the code is running from a jar
-			//openNlp throws a weird NumberFormatException if the temp file name doesn't end in .bin (!)			
-			InputStream is = ClassLoader.getSystemResourceAsStream("openNlpResources/tag.bin");
-			File tempFile = File.createTempFile("tag", ".bin");
-			tempFile.deleteOnExit();
-			OutputStream os = new FileOutputStream(tempFile);
-			IOUtils.copy(is, os);
-			
 			PosTagger posTagger = new PosTagger(tempFile.getCanonicalPath(), tagDict);
 			tags = posTagger.tag(token);
 			posContainer.addToBrownListFromStringArray(tags);
