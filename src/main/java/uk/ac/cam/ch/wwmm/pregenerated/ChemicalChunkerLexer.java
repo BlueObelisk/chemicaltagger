fb2cbf494688aba5b09c79492670017c9db12adb
lh359 <lh359@cam.ac.uk>
2010-11-29 08:40:59 +0000

    merged with Egon's version and works with the EvalutesAnnotations of the submitted paper

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk/ac/cam/ch/wwmm/chemicaltagger/ChemicalChunker.g 2010-11-09 00:29:35
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk/ac/cam/ch/wwmm/chemicaltagger/ChemicalChunker.g 2010-11-28 12:42:22
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
