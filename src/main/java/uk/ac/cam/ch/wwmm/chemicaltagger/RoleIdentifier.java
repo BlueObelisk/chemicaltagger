f24b53bd9bbe3ee521c7a8921fe2a06ecc366cb9
lh359 <lh359@localhost>
2010-12-11 17:25:27 +0000

    RoleIdentifier now returns HashMap<String,List<String>>

--- src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/RoleIdentifier.java
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/RoleIdentifier.java
@@ -3,7 +3,9 @@ package uk.ac.cam.ch.wwmm.chemicaltagger;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import nu.xom.Document;
 import nu.xom.Element;
@@ -16,19 +18,21 @@ public class RoleIdentifier {
 	private String text;
 
 	public RoleIdentifier() {
-		
+
 	}
 
-	public void setText(String text){
+	public void setText(String text) {
 		this.text = text;
 	}
-	public HashMap<String, String> getRoles() {
-		HashMap<String, String> roleMap = new HashMap<String, String>();
+
+	public HashMap<String, List<String>> getRoles() {
+		HashMap<String, List<String>> roleMap = new HashMap<String, List<String>>();
 		
 		Document parsedDoc = runChemicalTagger();
 		System.out.println(parsedDoc.toXML());
 		Nodes nodes = parsedDoc.query("//MOLECULE");
 		for (int i = 0; i < nodes.size(); i++) {
+			List<String> roles = new ArrayList<String>();
 			Element nodeElement = (Element) nodes.get(i);
 			String role = "None";
 			String chemicalName = "";
@@ -37,15 +41,17 @@ public class RoleIdentifier {
 			Nodes chemicalNodes = nodeElement.query(".//OSCAR-CM");
 			for (int j = 0; j < chemicalNodes.size(); j++) {
 				Element chemicalElement = (Element) chemicalNodes.get(j);
-				chemicalName = chemicalName +" "+chemicalElement.getValue();
+				chemicalName = chemicalName + " " + chemicalElement.getValue();
 			}
-	 
-			int count = 1;
-			while (roleMap.containsKey(chemicalName+"_"+String.valueOf(count)))
-				count = count + 1;
-			roleMap.put(chemicalName+"_"+String.valueOf(count), role);
+
+			if (roleMap.containsKey(chemicalName))
+				roles = roleMap.get(chemicalName);
+
+			roles.add(role);
+			roleMap.put(chemicalName, roles);
+
 		}
-		
+
 		return roleMap;
 	}
 
@@ -72,8 +78,8 @@ public class RoleIdentifier {
 
 	public static void main(String[] args) {
 		RoleIdentifier roleIdent = new RoleIdentifier();
-        roleIdent.setText("Potassium was washed with acetone. Salt in water");
-		HashMap<String, String> identifiedRoles = roleIdent.getRoles();
+		roleIdent.setText("Potassium was washed with acetone. Salt in water");
+		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles();
 		System.out.println(identifiedRoles);
 	}
 }
