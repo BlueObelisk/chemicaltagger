4e674bf4a909686bb1061ab4ee49a130cb566052
Egon Willighagen <egonw@users.noreply.github.com>
2010-12-12 20:08:26 +0100

    Strongly typed 'roles' to allow new details, such as the sentence in which the roles was found

---
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/NamedEntityWithRoles.java
@@ -0,0 +1,30 @@
+package uk.ac.cam.ch.wwmm.chemicaltagger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NamedEntityWithRoles {
+
+	private String namedEntity;
+	private List<Role> roles;
+
+	public NamedEntityWithRoles(String namedEntity) {
+		this.namedEntity = namedEntity;
+	}
+
+	public String getNamedEntity() {
+		return namedEntity;
+	}
+
+	public void addRole(Role role) {
+		if (roles == null) roles = new ArrayList<Role>();
+		roles.add(role);
+	}
+
+	public List<Role> getRoles() {
+		if (roles == null) return Collections.emptyList();
+		return Collections.unmodifiableList(roles);
+	}
+
+}
