896bb8c9161051bc16f3d9accc398d4dca9ec634
Egon Willighagen <egon.willighagen@gmail.com>
2010-12-12 20:08:26 +0100

    Strongly typed 'roles' to allow new details, such as the sentence in which the roles was found

---
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/Role.java
@@ -0,0 +1,29 @@
+package uk.ac.cam.ch.wwmm.chemicaltagger;
+
+public class Role {
+
+	private String role;
+	private String sentence;
+
+	public Role(String role, String sentence) {
+		this.setRole(role);
+		this.setSentence(sentence);
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+}
