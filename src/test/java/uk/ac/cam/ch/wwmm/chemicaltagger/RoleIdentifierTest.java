f8a79f63595bbb29381024ad670f4255e9ac3fca
Egon Willighagen <egonw@users.noreply.github.com>
2010-12-13 23:11:17 +0100

    Fixed unit tests for API changes

--- src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/RoleIdentifierTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/RoleIdentifierTest.java
@@ -3,14 +3,14 @@ package uk.ac.cam.ch.wwmm.chemicaltagger;
 import static uk.ac.cam.ch.wwmm.chemicaltagger.Utils.readSentence;
 
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.Collection;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.chemicaltagger.roles.NamedEntityWithRoles;
+import uk.ac.cam.ch.wwmm.chemicaltagger.roles.Role;
 import uk.ac.cam.ch.wwmm.chemicaltagger.roles.RoleIdentifier;
 
 public class RoleIdentifierTest {
@@ -21,7 +21,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest1() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence1.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 1, solventCount);
 		LOG.info(identifiedRoles);
@@ -32,7 +32,7 @@ public class RoleIdentifierTest {
 
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence2.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 1, solventCount);
 		LOG.info(identifiedRoles);
@@ -43,7 +43,7 @@ public class RoleIdentifierTest {
 
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence3.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 2, solventCount);
 		LOG.info(identifiedRoles);
@@ -53,7 +53,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest4() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence4.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 1, solventCount);
 		LOG.info(identifiedRoles);
@@ -63,7 +63,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest5() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence5.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 3, solventCount);
 		LOG.info(identifiedRoles);
@@ -73,7 +73,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest6() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence6.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 1, solventCount);
 		LOG.info(identifiedRoles);
@@ -83,7 +83,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest7() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence7.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 1, solventCount);
 		LOG.info(identifiedRoles);
@@ -93,7 +93,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest8() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence8.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 4, solventCount);
 		LOG.info(identifiedRoles);
@@ -103,7 +103,7 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest9() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence9.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 4, solventCount);
 		LOG.info(identifiedRoles);
@@ -113,19 +113,17 @@ public class RoleIdentifierTest {
 	public void RoleIdentifierTest10() throws UnsupportedEncodingException {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/roleIdentifierTest/Sentence10.txt");
 		RoleIdentifier roleIdent = new RoleIdentifier();
-		HashMap<String, List<String>> identifiedRoles = roleIdent.getRoles(sentence);
+		Collection<NamedEntityWithRoles> identifiedRoles = roleIdent.getRoles(sentence);
 		int solventCount = getSolventCount(identifiedRoles);
 		Assert.assertEquals("Solvent Count", 0, solventCount);
 		LOG.info(identifiedRoles);
 	}
 
-	private int getSolventCount(HashMap<String, List<String>> identifiedRoles) {
+	private int getSolventCount(Collection<NamedEntityWithRoles> identifiedRoles) {
 		int count = 0;
-		Set<String> keySet = identifiedRoles.keySet();
-		for (String key : keySet) {
-			List<String> roles = identifiedRoles.get(key);
-			for (String role : roles) {
-				if (!role.equals("None")) {
+		for (NamedEntityWithRoles entity : identifiedRoles) {
+			for (Role role : entity.getRoles()) {
+				if (!role.getRole().equals("None")) {
 					count += 1;
 					break;
 				}
