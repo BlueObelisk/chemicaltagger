a468ac3c8ced00a2e7afcbeac454bbdcdae046f8
Daniel Lowe <dl387@cam.ac.uk>
2011-03-28 19:08:00 +0100

    All tokens in Chemical Tagger's vocabulary can now be recognized as unmatchedTokens. Hence the bug whereby large sections of input could be absent from the resultant AST is corrected

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-03-28 18:48:29
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-03-28 19:00:00
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
