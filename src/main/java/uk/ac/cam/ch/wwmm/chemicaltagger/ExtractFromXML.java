9535afd2d1d15ae178da4ea88a22a29326badb7f
lh359 <lh359@cam.ac.uk>
2011-03-29 23:49:52 +0100

    added tests for extract content from xml

--- src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/ExtractFromXML.java
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/ExtractFromXML.java
@@ -1,14 +1,14 @@
 package uk.ac.cam.ch.wwmm.chemicaltagger;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Nodes;
 import nu.xom.ParsingException;
-
-import org.apache.log4j.Logger;
+import nu.xom.ValidityException;
 
 /********************************************
  * Extracts text from XML Patents
@@ -17,19 +17,20 @@ import org.apache.log4j.Logger;
  ********************************************/
 public class ExtractFromXML {
 
-	private static final Logger LOG = Logger.getLogger(ExtractFromXML.class);
 
 	/****************************************
 	 * Parses an XML files and saves the output into DocumentContainer
 	 * 
-	 * @param xmlTag (Element)
+	 * @param xmlTag
+	 *            (Element)
 	 * @return stringValue (String)
 	 ****************************************/
 	public static String getStringValue(Element xmlTag, String Delimiter) {
 		StringBuilder stringValue = new StringBuilder();
 		for (int i = 0; i < xmlTag.getChildCount(); i++) {
 			if (xmlTag.getChild(i) instanceof nu.xom.Text) {
-				stringValue.append(xmlTag.getChild(i).getValue().trim()+Delimiter);
+				stringValue.append(xmlTag.getChild(i).getValue().trim()
+						+ Delimiter);
 			} else {
 				if (!(xmlTag.getChild(i) instanceof nu.xom.ProcessingInstruction)) {
 					Element sub = (Element) xmlTag.getChild(i);
@@ -48,11 +49,11 @@ public class ExtractFromXML {
 		/***************
 		 * Replace the non-breaking space with a normal space
 		 */
-		return stringValue.toString().replace(" "," ").replace("=", "=").replace("   ", " ");
+		return stringValue.toString().replace(" ", " ").replace("=", "=")
+				.replace("   ", " ");
 	}
 
 	private static boolean hasMoreChildren(Element sub) {
-		
 
 		if (sub.getChildCount() == 0) {
 			return false;
@@ -62,61 +63,49 @@ public class ExtractFromXML {
 		}
 		return false;
 	}
-	
+
+
+
 	/*********************************
 	 * Returns the content of a searchTag from an XML File
-	 * @param sourceFile (String: Path to the file)
-	 * @param searchTag (String)
+	 * 
+	 * @param in (InputStream)
+	 * @param searchTag
+	 *            (String)
 	 * @return content (String)
+	 * @throws IOException 
+	 * @throws ParsingException 
+	 * @throws ValidityException 
 	 **********************************/
-	 public static String getContent(String sourceFile, String searchTag) {
-         Builder builder = new Builder();
-         Document doc = null;
-         String content = "";
+	public static String getContent(InputStream in, String searchTag) throws ValidityException, ParsingException, IOException {
 
-         LOG.info("Extracting data from " + sourceFile);
-         try {
-                 doc = builder.build(sourceFile);
-                 
-                 
-                 Nodes sections = doc.query(searchTag);
-                 System.out.println(sections.size());
-                 for (int i = 0; i < sections.size(); i++) {
+		Document doc = new Builder().build(in);
+		Nodes sections = doc.query(searchTag);
+		String content = "";
+		for (int i = 0; i < sections.size(); i++) {
 
-                         Element elementNode = (Element) sections.get(i);
+			Element elementNode = (Element) sections.get(i);
 
-                         String cleanNode = getStringValue(elementNode, "").trim()
-                                         .replace("\n", " ").replace("\r", "");
-                         ;
+			String cleanNode = getStringValue(elementNode, "").trim().replace(
+					"\n", " ").replace("\r", "");
+			
 
-                         if (cleanNode.toLowerCase().startsWith("tlc")) {
-                                 break;
-                         } else {
-                                 if (content.length() > 0) {
-                                         if (content.trim().endsWith(".")) {
-                                                 content += " " + cleanNode;
-                                         }
-                                         else{
-                                                 content += " . " + cleanNode;
-                                         }
-                                 } else
-                                         content = cleanNode;
-                         }
+			if (cleanNode.toLowerCase().startsWith("tlc")) {
+				break;
+			} else {
+				if (content.length() > 0) {
+					if (content.trim().endsWith(".")) {
+						content += " " + cleanNode;
+					} else {
+						content += " . " + cleanNode;
+					}
+				} else
+					content = cleanNode;
+			}
 
-                 }
+		}
 
-              
-
-         } catch (ParsingException ex) {
-                 LOG.fatal("ParsingException " + ex.getMessage(),
-                                 new RuntimeException());
-         } catch (IOException ex) {
-
-                 LOG.fatal(ex.getMessage(), new RuntimeException());
-         }
-         return content;
- }
-
-
+		return content;
+	}
 
 }
---
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/ExtractFromXMLTest.java
@@ -0,0 +1,26 @@
+package uk.ac.cam.ch.wwmm.chemicaltagger;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import junit.framework.Assert;
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
+
+import org.junit.Test;
+
+public class ExtractFromXMLTest {
+
+	@Test
+	public void testGetXMLContent() throws IOException, ValidityException,
+			ParsingException {
+
+		String path = "uk/ac/cam/ch/wwmm/chemicaltagger/xmlFiles/experiment.xml";
+		InputStream in = ClassLoader.getSystemResourceAsStream(path);
+		
+		String actual = ExtractFromXML.getContent(in, "//p");
+	    String expected = Utils.readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/xmlFiles/experimentContent");
+	    Assert.assertEquals("Extracted Content",expected, actual);
+	}
+
+}
