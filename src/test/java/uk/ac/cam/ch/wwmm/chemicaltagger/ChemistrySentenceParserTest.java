9a87345cf83f424f10efa6d0e8b6464233a8f0fe
Daniel Lowe <dl387@cam.ac.uk>
2011-03-29 17:02:31 +0100

    Enabled memoization. This yields significant speed improvements when backtracking is invoked ChemistrySentenceParserTest.java now checks that the resultant AST actually contains the same text as was input

--- src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/ChemistrySentenceParserTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/ChemistrySentenceParserTest.java
@@ -1,13 +1,15 @@
 package uk.ac.cam.ch.wwmm.chemicaltagger;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.Assert;
 import nu.xom.Document;
 
 import org.antlr.runtime.tree.Tree;
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /*****************************
@@ -20,111 +22,102 @@ import org.junit.Test;
 public class ChemistrySentenceParserTest {
 
 	@Test
-	public void testSentence1() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence1.txt");
+	public void testSentence1() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence1.txt");
 
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	public void testSentence2() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence2.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+	public void testSentence2() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence2.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	public void testSentence3() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence3.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+	public void testSentence3() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence3.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	public void testSentence4() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence4.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
-
+	public void testSentence4() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence4.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	public void testSentence5() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence5.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+	public void testSentence5() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence5.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	public void testSentence6() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence6.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+	public void testSentence6() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence6.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	public void testSentence7() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence7.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+	public void testSentence7() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence7.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
-	@Ignore //Currently takes far far too long to parse due to the run on sentence which is interpreted in a highly nested manner
-	public void testSentence8() {
-		InputStream taggedStream = getInputStream("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence8.txt");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				taggedStream);
+	public void testSentence8() throws IOException {
+		String input = getInputFromFile("uk/ac/cam/ch/wwmm/chemicaltagger/parseTest/sentence8.txt");
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(input);
 		chemChunkParser.parseTags();
 		Tree t = chemChunkParser.getParseTree();
-		checkNodes(t);
-		
+		checkForErrorNodes(t);
+		checkTreeText(t, input);
 	}
 
 	@Test
 	public void testEmptyInput() {
 		InputStream in = IOUtils.toInputStream("");
-		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(
-				in);
+		ChemistrySentenceParser chemChunkParser = new ChemistrySentenceParser(in);
 		chemChunkParser.parseTags();
 		Document doc = chemChunkParser.makeXMLDocument();
 		Assert.assertNull(doc);
 	}
 
-	private InputStream getInputStream(String resourceName) {
+	private String getInputFromFile(String resourceName) throws IOException {
 		InputStream inStream = this.getClass().getClassLoader()
 				.getResourceAsStream(resourceName);
-		return inStream;
+		return IOUtils.toString(inStream);
 	}
+	
+	
 
 	/*****************************
 	 * CheckNodes(Tree astTree)
@@ -133,7 +126,7 @@ public class ChemistrySentenceParserTest {
 	 *            Goes through the nodes of a tree and checks for unexpected
 	 *            tokens(when type=0)
 	 *****************************/
-	private void checkNodes(Tree astTree) {
+	private void checkForErrorNodes(Tree astTree) {
 		int nodeCount = astTree.getChildCount();
 		for (int i = 0; i < nodeCount; i++) {
 			String text = astTree.getChild(i).getText();
@@ -141,8 +134,42 @@ public class ChemistrySentenceParserTest {
 			// Log.info("****Text="+text+" type=="+type);
 			Assert.assertNotSame("Antlr Parse Fails for the for the text '"
 					+ text + "'", 0, type);
-			checkNodes(astTree.getChild(i));
+			checkForErrorNodes(astTree.getChild(i));
 
+		}
+	}
+	
+	/****************************
+	 * Checks whether the text children of the astTree are the same as the text present
+	 * in the tagged text.
+	 * 
+	 * @param astTree
+	 * @param taggedText
+	 ***************************/
+	private void checkTreeText(Tree astTree, String taggedText) {
+		List<String> textChildren = new ArrayList<String>();
+		List<String> referenceChildren = new ArrayList<String>();
+		getTextChildrenFromAst(astTree, textChildren);
+		String[] reference  =taggedText.split(" ");
+		for (int i = 0; i < reference.length; i++) {
+			if (i%2==1){
+				referenceChildren.add(reference[i]);
+			}
+		}
+		Assert.assertEquals(referenceChildren.size(), textChildren.size());
+		for (int i = 0; i < referenceChildren.size(); i++) {
+			Assert.assertEquals(referenceChildren.get(i), textChildren.get(i));
+		}
+	}
+
+	private void getTextChildrenFromAst(Tree astTree, List<String> textChildren) {
+		int nodeCount = astTree.getChildCount();
+		for (int i = 0; i < nodeCount; i++) {
+			Tree child = astTree.getChild(i);
+			getTextChildrenFromAst(child, textChildren);
+		}
+		if (nodeCount==0){
+			textChildren.add(astTree.getText());
 		}
 	}
 
