581777df9da83c93295326d629513a93335a50c2
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-04-27 15:56:26 +0100

    Phrases of the form quantity of quantity molecule e.g. 40ml of 3M sodium hydroxide will no longer be misidentified as unnamed molecules

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-04-26 15:35:20
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-04-27 15:51:52
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
