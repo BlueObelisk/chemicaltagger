65b0637120eec6cb022bb4f5fb046993c1060422
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-04-28 18:57:49 +0100

    Descriptions of the form molecule identifier quantities will now be detected as a molecule rather than a molecule followed by an unnamed molecule e.g. triol 3 (5g) Corrected unit test which expected the previous incorrect behaviour

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-04-27 15:51:52
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-04-28 18:28:25
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
