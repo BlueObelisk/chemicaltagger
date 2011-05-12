4b82999becb6a60aad809923b31d72cb6e838f1e
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-05-12 14:59:12 +0100

    measurements e.g. amount, volume etc. are now always enclosed in quantity elements Removed a few redundancies from the nounStructure expression Updated reference test result

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-05-11 20:04:57
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-05-12 14:35:33
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
