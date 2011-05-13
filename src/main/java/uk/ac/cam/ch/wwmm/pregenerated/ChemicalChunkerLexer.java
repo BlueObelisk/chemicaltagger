4f391aa877a00a4ed664e915a1089a226f069f04
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-05-13 14:03:44 +0100

    Increased recall of regex expressions for quantities The fact that quantities concatenated with numbers are split by the Formatter.java has been made more explicit by not having quantity regexes match digits Expression of MOLAR is now consistent with other quantities. Updated unit test accordingly

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-05-12 14:35:33
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-05-13 13:47:56
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
