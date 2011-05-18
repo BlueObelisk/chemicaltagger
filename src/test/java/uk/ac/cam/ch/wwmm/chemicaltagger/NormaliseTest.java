eecbfe5dcd5b46710cb24d12cb81aa5791624ea8
Daniel Lowe <dl387@cam.ac.uk>
2011-05-18 18:34:20 +0100

    Split concatentated pH terms apart e.g. pH6.5 --> pH 6.5

--- src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/NormaliseTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/NormaliseTest.java
@@ -2,7 +2,7 @@ package uk.ac.cam.ch.wwmm.chemicaltagger;
 
 import static uk.ac.cam.ch.wwmm.chemicaltagger.Utils.readSentence;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /***********************************************
@@ -18,9 +18,29 @@ public class NormaliseTest {
 		String sentence = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/formatTest/sentence1.txt");
         String cleanSentence = Formatter.normaliseText(sentence);
 		String ref = readSentence("uk/ac/cam/ch/wwmm/chemicaltagger/formatTest/ref1.txt");
-		Assert.assertEquals(ref,cleanSentence);
-
-
+		assertEquals(ref,cleanSentence);
+	}
+	
+	
+	@Test	
+	public void testAmountSplitting() {
+		assertEquals("5 gram", Formatter.normaliseText("5gram"));
+		assertEquals("5 M", Formatter.normaliseText("5M"));
+		assertEquals("5 ml", Formatter.normaliseText("5ml"));
+		assertEquals("3 dm3", Formatter.normaliseText("3dm3"));
+		assertEquals("7 cm3", Formatter.normaliseText("7cm3"));
+		assertEquals("4.5 eq", Formatter.normaliseText("4.5eq"));
+		assertEquals("5.3 g", Formatter.normaliseText("5.3g"));
+		assertEquals("5g", Formatter.normaliseText("5g"));//could be an identifier
+		assertEquals("5 L", Formatter.normaliseText("5L"));
+		assertEquals("5l", Formatter.normaliseText("5l"));//could be an identifier
+	}
+	
+	@Test	
+	public void testpHSplitting() {
+		String normalisedText = Formatter.normaliseText("pH6.5");
+		assertEquals("pH 6.5", normalisedText);
+		assertEquals("pHenyl", Formatter.normaliseText("pHenyl"));
 	}
 	
 }
