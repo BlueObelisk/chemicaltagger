5fc1c0e3b50dc450f00ae968bae54f690d402efa
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-05-20 16:24:36 +0100

    Allowed greater variety of numeric references in unnamed molecules and expressions like 50g of compound 2.

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-05-20 15:08:45
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-05-20 16:22:57
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
