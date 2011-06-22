a0dc9088cb8f6ae4eb8a60ca3c34a70edd93961a
lh359 <lh359@localhost>
2011-06-22 16:29:02 +0100

    moved from opennlp1.4 to opennlp1.5

--- src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/OpenNLPTagger.java
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/OpenNLPTagger.java
@@ -1,16 +1,16 @@
 package uk.ac.cam.ch.wwmm.chemicaltagger;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.List;
 
-import opennlp.tools.lang.english.PosTagger;
-import opennlp.tools.postag.POSDictionary;
+import opennlp.tools.cmdline.postag.POSModelLoader;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTagger;
+import opennlp.tools.postag.POSTaggerME;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
@@ -27,21 +27,19 @@ public class OpenNLPTagger {
 	private static class INSTANCE_HOLDER {
 		private static OpenNLPTagger myInstance = new OpenNLPTagger();
 	}
-
-	private File tempFile;
-	private PosTagger posTagger;
-	private static Logger LOG = Logger.getLogger(OscarTagger.class);
+	POSTagger posTagger;
+	private static Logger LOG = Logger.getLogger(OpenNLPTagger.class);
 
 	/**************************************
 	 * Private Constructor Class.
 	 ***************************************/
 	private OpenNLPTagger() {
+
+		
 		try {
-			setUpTagDict();
 			setUpPosTagger();
 		} catch (IOException e) {
-			LOG.error("Exception " + e.getMessage());
-			throw new RuntimeException("Failed to initialise PosTagger", e);
+			e.printStackTrace();
 		}
 	}
 
@@ -58,43 +56,22 @@ public class OpenNLPTagger {
 	 * 
 	 * @return posTagger(PosTagger).
 	 ***************************************/
-	public PosTagger getTagger() {
+	public POSTagger getTagger() {
 		return posTagger;
 	}
 
 
-    /**************************************
-     * Copies the tagDict to a temporary location.
-     * Workaround method because the POSTagger 
-     * constructor only takes a filepath.
-     ***************************************/
-    private void setUpTagDict() {
-            try {
-                    
-                    // openNlp throws a weird NumberFormatException if the temp file
-                    // name doesn't end in .bin (!)
-                    tempFile = File.createTempFile("tag", ".bin");
-                    tempFile.deleteOnExit();
-                    InputStream is = getClass().getClassLoader().getResourceAsStream(
-                                    "uk/ac/cam/ch/wwmm/chemicaltagger/openNLPTagger/tag.bin");
-                    OutputStream os = new FileOutputStream(tempFile);
-                    IOUtils.copy(is, os);
-            } catch (IOException e) {
-                    LOG.error("Exception " + e.getMessage());
-                    e.printStackTrace();
-            }
-    }
+
     
     /**************************************
      * Loads the Penn Treebank tagset to POSTagger.
 	 * @throws IOException
 	 * ***************************************/
 	private void setUpPosTagger() throws IOException {
-		InputStreamReader tagDictReader = null;
-		InputStream in = Utils.getInputStream(getClass(),"/uk/ac/cam/ch/wwmm/chemicaltagger/openNLPTagger/tagdict");
-		tagDictReader = new InputStreamReader(in);
-		POSDictionary tagDict = new POSDictionary(new BufferedReader(tagDictReader), true);
-		posTagger = new PosTagger(tempFile.getCanonicalPath(), tagDict);
+        InputStream is = getClass().getClassLoader().getResourceAsStream(
+        "uk/ac/cam/ch/wwmm/chemicaltagger/openNLPTagger/en-pos-maxent.bin");
+        POSModel model = new POSModel(is);
+		posTagger = new POSTaggerME(model);
 	}
 
 	/*****************************************************
