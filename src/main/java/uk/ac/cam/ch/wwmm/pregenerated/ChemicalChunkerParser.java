892e427b1896931c27dc8a56eae1bf4bd7ec35f3
lh359 <lh359@cam.ac.uk>
2011-06-29 16:17:55 +0100

    added the apache license v2

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerParser.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerParser.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 Lezan Hawizy, David M. Jessop, Daniel Lowe and Peter Murray-Rust
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-06-19 13:44:43
 
     package uk.ac.cam.ch.wwmm.pregenerated;
