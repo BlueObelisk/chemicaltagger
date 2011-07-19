e7ee4153ca01eab6d163cadedb77ba629d7bad4b
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-07-19 18:19:40 +0100

    Slightly simplified expression for dissolveStructure to avoid problems where the (conjunction molecule)* condition was thrawting detection as a dissolvePhrase

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-07-19 16:24:35
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-07-19 18:11:02
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
