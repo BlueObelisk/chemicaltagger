3548d80930ee0317cc41f0e7aea92f19d4f12b5a
Daniel Lowe <dl387@cam.ac.uk>
2011-07-21 19:32:50 +0100

    Allowed a quantity after a reference to a compound Added reagent/reactant/vicinal to look out for

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-07-20 19:38:39
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-07-21 19:26:27
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
