6970925f2d15803e0ed65f4fdfbb07c1ca57b1fe
Daniel Lowe <dl387@cam.ac.uk>
2011-10-05 15:50:36 +0100

    Fixed a bracketted ratio that contained other things not being identified as a ratio for two compounds

--- src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
+++ src/main/java/uk/ac/cam/ch/wwmm/pregenerated/ChemicalChunkerLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-09-26 21:27:51
+// $ANTLR 3.2 Sep 23, 2009 14:05:07 uk\\ac\\cam\\ch\\wwmm\\pregenerated\\ChemicalChunker.g 2011-10-05 15:28:53
 package uk.ac.cam.ch.wwmm.pregenerated;
 
 import org.antlr.runtime.*;
