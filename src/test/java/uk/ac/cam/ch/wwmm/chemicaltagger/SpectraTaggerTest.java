db792ac3713cebe4999831345696e9c396b48c6c
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2011-11-02 17:26:22 +0000

    Corrected tagged spectra not being removed from inputText

--- src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/SpectraTaggerTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/chemicaltagger/SpectraTaggerTest.java
@@ -28,8 +28,6 @@ import org.junit.Test;
  ***********************************************/
 public class SpectraTaggerTest {
 
-
-
 	@Test
 	public void sentence1() {
 		String text = "The title compound was synthesized from ethyl 3-cyclopropyl-4H-furo[3,2-b]pyrrole-5-carboxylate (110 mg, 0.50 mmol) according to General Procedure 2 and was purified by flash chromatography (Isco CombiFlash, 0-60% EtOAc/heptane) to afford 3-cyclopropyl-4H-furo[3,2-b]pyrrole-5-carboxylic acid 31 (34 mg, 35%). 1H NMR (400 MHz, CD3OD) \u03B4 ppm 0.67-0.72 (m, 2H), 0.86-0.92 (m, 2H), 1.75-1.84 (m, 1H), 6.64 (s, 1H), 7.34 (d, J=0.83 Hz, 1H); LCMS-MS (ESI\u2212) 189.8 (M\u2212H); HPLC (UV=95.9%), (ELSD=100%). ";
@@ -42,8 +40,7 @@ public class SpectraTaggerTest {
         doc.getRootElement().appendChild(posContainer.getSpectrumElementList());
         Assert.assertEquals("SpectrumList size:", 1, doc.query("//SpectrumList").size());
 		Assert.assertEquals("Spectrum size:", 3, doc.query("//spectrum").size());
-		
+		Assert.assertEquals("Modified input text length", text.length() - posContainer.getSpectrumElementList().getValue().length(), posContainer.getInputText().length());
 
 	}
-
 }
